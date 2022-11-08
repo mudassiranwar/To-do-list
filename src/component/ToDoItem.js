ffb16b5aab27fb05de09fe080bec74c337973afd
shahidshah95 <shahidssd3@gmail.com>
2022-11-08 14:38:48 +0530

    checkbox fixed

--- src/component/ToDoItem.js
+++ src/component/ToDoItem.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./ToDoItem.module.css";
 
 export default function ToDoItem(props) {
   let taskDone = props.currentToDo.checked;
   return (
-    <>
+    <Fragment>
       <div
         className={`d-flex align-items-center justify-content-between my-4 p-3 bg-white ${classes.card}`}
       >
         <div className={`d-flex ${taskDone && classes.disabled}`}>
           <input type="checkbox" id={props.currentToDo.No} checked={props.currentToDo.checked} onChange={props.onChange} className="mx-2" />
-          <label htmlFor={props.currentToDo.No} className={`m-0`}>
+          <label className={`m-0`}>
             {props.currentToDo.massage}
           </label>
         </div>
@@ -18,6 +18,6 @@ export default function ToDoItem(props) {
           delet
         </button>
       </div>
-    </>
+    </Fragment>
   );
 }
